52fbe0f085c6ef655d73ed114b1cc7815bbe4fc5
Fluff18 <rahulkoushik1999@gmail.com>
2022-11-16 20:35:08 -0700

    Finished the initial set of screens

--- Fetch Recipes/Fetch Recipes/Views/Onboarding/OnboardingViewController.swift
+++ Fetch Recipes/Fetch Recipes/Views/Onboarding/OnboardingViewController.swift
@@ -15,6 +15,17 @@ class OnboardingViewController: UIViewController {
     
     
     var slide: [OnboardingSlide] = []
+    var currentPage = 0 {
+        didSet {
+            if currentPage == slide.count - 1 {
+                nextButton.setTitle("Get Started", for: .normal)
+            }
+            else {
+                nextButton.setTitle("Next", for: .normal)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,13 +33,24 @@ class OnboardingViewController: UIViewController {
         collectionView.dataSource = self
         
         slide = [OnboardingSlide(title: "Find everyday cooking Inspiration", description: "Welcome to Fetch Recipes!", image: UIImage(named: "Food")!),
-                 OnboardingSlide(title: "Become World-Class Chefs", description: "Let's get started!", image: UIImage(named: "Food2")!)]
+                 OnboardingSlide(title: "Lets get started, Chef!", description: "ANYONE CAN COOK", image: UIImage(named: "Food2")!)]
+        pageControl.numberOfPages = slide.count
     }
     
     @IBAction func nextButtonClicked(_ sender: UIButton) {
+        if(currentPage == slide.count - 1) {
+            let controller = storyboard?.instantiateViewController(identifier: "HomeNavigationController") as! UINavigationController
+            controller.modalPresentationStyle = .fullScreen
+            controller.modalTransitionStyle = .flipHorizontal
+            present(controller, animated: true, completion: nil)
+        }
+        
+        else {
+                currentPage = 1
+                let indexPath = IndexPath(item: currentPage, section: 0)
+                collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
     }
-    
-
 }
 
 extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -43,6 +65,11 @@ extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDa
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x/width)
+        pageControl.currentPage = currentPage
     }
 }
 

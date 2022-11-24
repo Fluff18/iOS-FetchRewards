4c8456b556504de635bc5f9c94d155373dff6353
Fluff18 <rahulkoushik1999@gmail.com>
2022-11-23 17:02:48 -0700

    IDK HOW but its working '

--- Fetch Recipes/Fetch Recipes/CustomViews/CollectionViewCells/FoodCategoryCollectionViewCell/FoodCategoryCollectionViewCell.swift
+++ Fetch Recipes/Fetch Recipes/CustomViews/CollectionViewCells/FoodCategoryCollectionViewCell/FoodCategoryCollectionViewCell.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class FoodCategoryCollectionViewCell: UICollectionViewCell {
-
-    static let identifier = String(describing: FoodCategoryCollectionViewCell.self)
-    
     @IBOutlet weak var FoodCategoryTitle: UILabel!
     @IBOutlet weak var FoodCategoryImage: UIImageView!
     
+    static let identifier = String(describing: FoodCategoryCollectionViewCell.self)
+    
+    
     func setup(foodcategory: FoodCategory) {
         FoodCategoryTitle.text = foodcategory.strCategory
-//        FoodCategoryImage.kf.setImage(with: foodcategory.strCategoryThumb?.asUrl)
+        FoodCategoryImage.kf.setImage(with: foodcategory.strCategoryThumb?.asUrl)
     }
     
 }

c09b49ed06632d7abeebbe73298681e2fa142926
Fluff18 <rahulkoushik1999@gmail.com>
2022-11-26 18:23:26 -0700

    Add ingredients somehow

--- Fetch Recipes/Fetch Recipes/Views/Recipe/RecipeViewController.swift
+++ Fetch Recipes/Fetch Recipes/Views/Recipe/RecipeViewController.swift
@@ -11,6 +11,7 @@ class RecipeViewController: UIViewController {
     @IBOutlet weak var RecipeInstructionsView: UILabel!
     @IBOutlet weak var MealTitle: UILabel!
     @IBOutlet weak var RecipeImageView: UIImageView!
+    @IBOutlet weak var tagTitle: UILabel!
     var selectedRecipe: GetRecipe!
     var Recipes: [GetRecipe] = []
     override func viewDidLoad() {
@@ -28,6 +29,8 @@ class RecipeViewController: UIViewController {
                 self.Recipes = data.map({ GetRecipe.init(idMeal: $0.idMeal, strMeal: $0.strMeal, strCategory: $0.strCategory, strArea: $0.strArea, strInstructions: $0.strInstructions, strTags: $0.strTags, strDrinkAlternate: $0.strDrinkAlternate, strMealThumb: $0.strMealThumb, strYoutube: $0.strYoutube, strIngredient1: $0.strIngredient1, strIngredient2: $0.strIngredient2, strIngredient3: $0.strIngredient3, strIngredient4: $0.strIngredient4, strIngredient5: $0.strIngredient5, strIngredient6: $0.strIngredient6, strIngredient7: $0.strIngredient7, strIngredient8: $0.strIngredient8, strIngredient9: $0.strIngredient9, strIngredient10: $0.strIngredient10, strIngredient11: $0.strIngredient11, strIngredient12: $0.strIngredient12, strIngredient13: $0.strIngredient13, strIngredient14: $0.strIngredient14, strIngredient15: $0.strIngredient15, strIngredient16: $0.strIngredient16, strIngredient17: $0.strIngredient17, strIngredient18: $0.strIngredient18, strIngredient19: $0.strIngredient19, strIngredient20: $0.strIngredient20, strSource: $0.strSource, strImageSource: $0.strImageSource, strCreativeCommonsConfirmed: $0.strCreativeCommonsConfirmed, dateModified: $0.dateModified)})
                 self.RecipeImageView.kf.setImage(with: data[0].strMealThumb?.asUrl)
                 self.RecipeInstructionsView.text = data[0].strInstructions
+                self.MealTitle.text = data[0].strMeal
+                self.tagTitle.text = data[0].strTags
             case .failure(let error):
                 print("The error is \(error.localizedDescription)")
             }
